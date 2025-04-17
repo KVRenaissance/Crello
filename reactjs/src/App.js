eddc060ea4af0ec0c5293d201ed081d18b893ffc
KVRenaissance <kyle@roguex.co.za>
2025-04-17 15:15:06 +0200

    Second commit: Built functional web prototype

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -2,10 +2,64 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello World with React and RogueX!
-      </h1>
+    <div className="crello-master">
+      <nav className="navbar app"> App bar</nav>
+      <nav className="navbar board"> Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://github.com/haryphamdev/sharing-host-files/blob/master/trello/img-design.png?raw=true "/>
+              Design and Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>fourth</li>
+            <li>fifth</li>
+            <li>sixth</li>
+            <li>seventh</li>
+            <li>eighth</li>
+            <li>ninth</li>
+            <li>tenth</li>
+            <li>eleventh</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://github.com/haryphamdev/sharing-host-files/blob/master/trello/img-design.png?raw=true "/>
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://github.com/haryphamdev/sharing-host-files/blob/master/trello/img-design.png?raw=true "/>
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://github.com/haryphamdev/sharing-host-files/blob/master/trello/img-design.png?raw=true "/>
+              Design and Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
